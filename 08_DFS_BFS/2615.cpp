304309333f6a824a48ab15a27d10ac337108685c
eugene385 <eugene03850@gmail.com>
2024-04-08 20:41:08 +0900

    [DFS & BFS] 4월 8일

---
+++ 08_DFS_BFS/2615.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int back_x, back_y;
+
+bool inBoard(int x, int y){
+    if(x>=0 && x<19 && y>=0 && y<19){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool findFive(int x, int y, vector<vector<int>>& set){
+    int dx[4] = {1,0,-1,1};
+    int dy[4] = {0,1,1,1};
+    
+    if(set[x][y]==0){
+        return false;
+    }
+
+    
+    for(int i=0; i<4; i++){
+        
+
+        int cnt=1;
+
+        int next_x = x+dx[i];
+        int next_y = y+dy[i];
+        
+        while(inBoard(next_x, next_y)&&(set[x][y]==set[next_x][next_y])){
+            cnt++;
+            next_x+=dx[i];
+            next_y+=dy[i];
+        }
+        
+        back_x = x-dx[i];
+        back_y = y-dy[i];
+        
+        while(inBoard(back_x, back_y) && (set[back_x][back_y]==set[x][y])){
+            cnt++;
+            back_x-=dx[i];
+            back_y-=dy[i];
+        } 
+
+        back_x+=dx[i];
+        back_y+=dy[i];
+        
+
+        if(cnt == 5){
+            return true;
+        }
+            
+
+    }
+    return false;
+
+
+}
+
+int main(){
+    
+    vector<vector<int>> set(19, vector<int>(19,0));
+    
+    for(int i=0; i<19; i++){
+        for(int j=0; j<19; j++){
+            cin>>set[i][j];
+        }
+    }
+    
+    for(int i=0; i<19; i++){
+        for(int j=0; j<19; j++){
+            if(findFive(i, j, set)){
+                cout<<set[i][j]<<endl;
+                cout<<back_x+1<<" "<<back_y+1;
+                return 0;
+            }
+        }
+    }
+    
+    cout<<0<<endl;
+    
+    return 0;
+}
