47aabc4e018ab30d82062175f0b8ca76c2685e5a
eugene385 <eugene03850@gmail.com>
2024-05-28 11:27:38 +0900

    [최단경로] 5월 28일 -ing

--- 13_최단경로/15685.cpp
+++ 13_최단경로/15685.cpp
@@ -13,6 +13,8 @@ const int SIZE = 101;
 3세대: 0 1 2 1 2 3 2 1
 */
 
+//규칙성에 따라 드래곤커브가 움직일 방향을 설정
+//n세대: (n-1세대) concat reverse((n-1세대 각 방향+1)%4)
 vector<int> generations(int g, vector<int>& dir){
     for(int gen = 0; gen < g; ++gen) {
         int size = dir.size();
@@ -29,22 +31,22 @@ int check(vector<vector<int>>& board){
         for(int j = 0; j < SIZE-1; ++j){
             if (board[i][j] && board[i + 1][j] && board[i][j + 1] && board[i + 1][j + 1]) {
                 ans++;
-            }
+            } //사각형 만들어지는 조건 확인
         }
     }
     return ans;
 }
 
 void dragonCurve(int x, int y, vector<int>& dir, vector<vector<int>>& board){
-    int dx[4] = {1, 0, -1, 0};
+    int dx[4] = {1, 0, -1, 0}; //→ ↑ ← ↓
     int dy[4] = {0, -1, 0, 1};
 
-    board[y][x] = 1;
+    board[x][y] = 1;
 
-    for(int d : dir){
+    for(int d : dir){ //dir에 저장된 방향대로 1씩 늘리기
         x += dx[d];
         y += dy[d];
-        board[y][x] = 1;
+        board[x][y] = 1;
     }
 }
 
